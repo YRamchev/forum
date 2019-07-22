fa050358412aa145f6a2879fa20effb61342e5f2
Yordan Ramchev <dakata911@gmail.com>
2019-07-22 16:42:05 +0300

    add style ryles

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,33 @@
 <template>
-  <div>
-    <h1>Welcome to the Forum</h1>
-    <div v-for="thread in threads" :key="thread.key">
-      <h2>{{ thread.title }}</h2>
-      <div v-for="postId in thread.posts" :key="postId">
-        <p>{{users[posts[postId].userId].name}}</p>
-        <p>{{posts[postId].text}}</p>
+<div>
+  <div v-for="thread in threads" :key="thread.key" class="col-large push-top">
+    <h1>{{ thread.title }}</h1>
+    <div class="post-list">
+      <div v-for="postId in thread.posts" :key="postId" class="post">
+        <div class="user-info">
+          <a href="" class="user-name">{{users[posts[postId].userId].name}}</a>
+          <a href="">
+            <img :src="users[posts[postId].userId].avatar" alt="" class="avatar-large">
+          </a>
+          <p class="desktop-only text-small">107</p>
+        </div>
+        <div class="post-content">
+          <div>
+            {{ posts[postId].text}}
+          </div>
+        </div>
+        <div class="post-date text-faded">
+          {{posts[postId].publishedAt}}
+        </div>
       </div>
     </div>
   </div>
+</div>
 </template>
 
 <script>
 import sourceData from '@/data'
-console.log(sourceData)
+
 export default {
   name: 'HelloWorld',
   data () {
@@ -25,3 +39,7 @@ export default {
   }
 }
 </script>
+
+<style>
+  
+</style>
