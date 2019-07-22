611f55acf1786fd84a53e22e0ff0b0079790c546
Yordan Ramchev <dakata911@gmail.com>
2019-07-22 15:13:16 +0300

    init project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Welcome to the Forum</h1>
+    <div v-for="thread in threads" :key="thread.key">
+      <h2>{{ thread.title }}</h2>
+      <div v-for="postId in thread.posts" :key="postId">
+        <p>{{users[posts[postId].userId].name}}</p>
+        <p>{{posts[postId].text}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data'
+console.log(sourceData)
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  }
+}
+</script>
